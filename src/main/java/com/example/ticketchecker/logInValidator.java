0d2361f8ab3cd0cedb28ea7b98e0eedbbf40e027
RishiSarraff <sarraff2004@gmail.com>
2024-05-12 20:02:22 -0400

    Latest updates

--- src/main/java/com/example/ticketchecker/logInValidator.java
+++ src/main/java/com/example/ticketchecker/logInValidator.java
@@ -29,17 +29,21 @@ public class logInValidator {
                 if(newOrOldUser == 0) {
                     db.insertUser(username, password);
                     db.commit();
-                    dialogPopUp("Created Account Successfully", "Please Proceed");
+                    dialogPopUp("Created account successfully", "Please proceed");
                     return true;
                 }
                 else{
-                    dialogPopUp("Username or Password is Wrong, Please Retry", "Retry Information Entry");
+                    dialogPopUp("Username or password is wrong, please retry", "Retry information entry");
                     return false;
                 }
             }
-            else{
-                dialogPopUp("User Exists and Can Now Log In", "Success");
+            else if(db.findUser(username, password)){
+                dialogPopUp("User exists and can now log in", "Success");
                 return true;
+            }
+            else{
+                dialogPopUp("User could not be found", "Please retry");
+                return false;
             }
 
         } catch (SQLException e) {
@@ -50,27 +54,33 @@ public class logInValidator {
 
     public static boolean validatePIN(String pin){
         pin = pin.trim();
+        if(pin.length() == 0){
+            dialogPopUp("You did not enter a PIN, please enter one", "No input provided");
+            return false;
+        }
         for(int i = 0; i<pin.length();i++){
             Character currChar = pin.charAt(i);
             if(!Character.isDigit(currChar)){
-                dialogPopUp("Invalid PIN, you must type in only digits", "Retry PIN Entry");
+                dialogPopUp("Invalid PIN, you must type in only digits", "Retry PIN entry");
                 return false;
             }
         }
 
+
+
         int userInput = Integer.parseInt(pin);
         if(userInput == realPin){
-            dialogPopUp("Correct Pin Was Entered, You May Proceed", "Success");
+            dialogPopUp("Correct Pin was entered, you may proceed", "Success");
             return true;
         }
 
-        dialogPopUp("Wrong Pin Number, Try Again", "Invalid Number Entry");
+        dialogPopUp("Wrong Pin number, try again", "Invalid number entry");
         return false;
     }
 
     public static boolean validatePassword(String password){
         if(password.length() < 8){
-            dialogPopUp("Password is not allowed, must be at least 8 characters long", "Retry password");
+            dialogPopUp("Password is not allowed, must be at least 8 characters long", "Try again");
             return false;
         }
         int countOfDigits = 0;
@@ -80,10 +90,14 @@ public class logInValidator {
             if(Character.isDigit(currChar)){
                 countOfDigits++;
             }
+            if(!Character.isDigit(currChar) || !Character.isLetter(currChar)){
+                dialogPopUp("Password can only be letters and digits", "Try again");
+                return false;
+            }
         }
 
         if(countOfDigits <=0){
-            dialogPopUp("Password must contain at least 1 digit", "Retry again");
+            dialogPopUp("Password must contain at least 1 digit", "Try again");
             return false;
         }
 
