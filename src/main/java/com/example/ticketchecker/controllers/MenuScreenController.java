151f88120a234c3c899f73e8b08fb7eebfd2112a
RishiSarraff <sarraff2004@gmail.com>
2024-05-19 17:04:32 -0400

    created menu screen controller and tried to make navigation bar expand

--- src/main/java/com/example/ticketchecker/controllers/MenuScreenController.java
+++ src/main/java/com/example/ticketchecker/controllers/MenuScreenController.java
@@ -1,4 +1,22 @@
 package com.example.ticketchecker.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+
 public class MenuScreenController {
+
+    @FXML
+    private HBox navigationHBox;
+
+    @FXML
+    private StackPane navigationPane;
+
+    @FXML
+    void initialize(){
+
+        HBox.setHgrow(navigationHBox, Priority.ALWAYS);
+
+    }
 }
